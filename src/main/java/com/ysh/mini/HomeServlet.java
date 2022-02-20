a907bf2a3eb652268fc760940306a27ceb94bef5
seonghuncode <gnos456@naver.com>
2022-02-20 21:20:46 +0900

    servlet으로 JSP Response

--- src/main/java/com/ysh/mini/HomeServlet.java
+++ src/main/java/com/ysh/mini/HomeServlet.java
@@ -1,8 +1,8 @@
 package com.ysh.mini;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -11,32 +11,17 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/home")
 public class HomeServlet extends HttpServlet {
-	
+	//프로그램 과정 : home아라는 요청을 했을때 -> /WEB-INF/intex.jsp 응답 ->
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//클라이언트에 대한 요청 <- request ->  클라이언트에 대한 응답
-		String number = request.getParameter("number");
 		
-		int result = Integer.parseInt(number) * 5;
+		//ctrl + 1 단축키 ->  inmport
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/WEB-INF/index.jsp");
+		//==> home으로 들어욌을때 /WEB-INF/intex.jsp 여기를 보여 달라는 의미(jsp는 WEB-INF안에 만들어 주어야 한다)
+		//RequestDispatcher == 요청을 발송해주는 역할(url로들어온 요청을 WEB-INF/intex.jsp 여기로 보여준다.)
 		
-		response.setCharacterEncoding("UTF-8");//컴퓨터는 한글을 잘 못읽기 때문에 잘 이해할 수 있도록 적어준는 것
-		response.setContentType("text/html; charset=UTF-8");//한글로 잘 넣어 달라고 써준는 것
-		
-		PrintWriter write = response.getWriter(); //웹에서 결과를 보여주기 위한 코드
-		//response의 바디에 넣는 것을 보여준느 것
-		
-		write.println("<!DOCTYPE html>"
-				+ "<html lang='ko'>"
-				+ "<head>"
-				+ "<meta charset='UTF-8'>"
-				+ "<title>setvlet</title>"
-				+ "<body>"
-				+ "<hi>"
-				+ "보내주신 숫자에 5를 곱한 결과는"
-				+ result
-				+ "입니다."
-				+ "</h1>"
-				+ "</body>"
-				+ "</html>");
+		requestDispatcher.forward(request, response);
+		//forward메서는 == WEB-INF/intex.jspdurlfh 여기로
 		
 	}
 
